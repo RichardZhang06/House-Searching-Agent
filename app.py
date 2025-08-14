70f81684f4c78655cbefd67a9968d30636df59e7
RichardZhang06 <richardzhangpt@gmail.com>
2025-08-13 23:41:56 -0400

    change to flask

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from flask import Flask
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/")
-def read_root():
-    html_content = """
-    <html>
-        <head>
-            <title>Hello Azure</title>
-        </head>
-        <body>
-            <h1>Hello from FastAPI on Azure!</h1>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content)
+@app.route('/')
+def home():
+    return "Hello, Azure!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
